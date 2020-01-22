240c0a45d3fbef4c2ce5213814127f0898303574
janghyosun <zzangyo@encar.com>
2020-01-22 14:31:35 +0900

    todolist component 작성

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
 import React from 'react';
+import { createGlobalStyle } from 'styled-components';
+import TodoTemplate from './components/TodoTemplate';
+import TodoHead from './components/TodoHead';
+import TodoList from './components/TodoList';
+import TodoCreate from './components/TodoCreate';
 
-function App() {
-	return (
-		<div className="App" />
-	);
-}
+const GlobalStyle = createGlobalStyle`
+  body {
+  background: #e9ecef;
+`;
+
+const App = () => (
+	<>
+		<GlobalStyle />
+		<TodoTemplate>
+			<TodoHead />
+			<TodoList />
+			<TodoCreate />
+		</TodoTemplate>
+	</>
+);
 
 export default App;
